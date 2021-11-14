ded9bd09e3e2980892826bc8975824dac81ecab5
JackieAlacrity2373 <94094054+JackieAlacrity2373@users.noreply.github.com>
2021-11-14 10:07:29 +0000

    Update Fizzbuzz.cpp
    Added a view counter for the total number of each fizzes and buzzes.

--- Fizzbuzz.cpp
+++ Fizzbuzz.cpp
@@ -11,10 +11,12 @@ int main()
     
      //get the number from the user
     int N;
+    int fcount=0,bcount=0,fbcount=0;
     std::cin >> N;
     //iterate through all values until reaching the maximum value of N
     for(int i=0; i < N; i++)
-        //checks for zero and places lets play fizzbuzz in its place
+        {
+	//checks for zero and places lets play fizzbuzz in its place
         if (i == 0)
         {
             cout << "lets play fizzbuzz to " << N-- << '\n';
@@ -25,22 +27,29 @@ int main()
         if (i % 5 == 0 && i % 3 == 0)
         {
             std::cout << "fizzbuzz" << '\n';
+		fcount++;
+		bcount++;
+		fbcount++;
         }
         else
         //checks for fizz case
         if (i % 3 == 0)
         {
             std::cout << "fizz" << '\n';
-             
+             fcount++;
         }
         else
         //checks for buzz case 
         if (i % 5 == 0)
         {
             std::cout << "buzz" << '\n';
+	     bcount++;
         }
         else
         //if no case is met output number
         std::cout << i << '\n';
+	 
+	}
+     std::cout << "fizzes: " << fcount << '\n' << "buzzes: " << bcount << '\n' << "fizzbuzzes: " << fbcount;
     return 0;
 }
