4dcbcd22e19ca7f0cad6177ad0b2b6c2e4fa6e04
JackieAlacrity2373 <94094054+JackieAlacrity2373@users.noreply.github.com>
2022-01-18 21:30:09 -0800

    Update fizzbuzzX.cpp

--- fizzbuzzX.cpp
+++ fizzbuzzX.cpp
@@ -6,14 +6,18 @@ using namespace std;
 
 int main()
 {
-    //prompt user 
-    cout<<"Select a number\n";
-    
+    //prompt user
+    cout<<"Lets play fizzbuzz"  << '\n';
+
      //get the number from the user
-    int N;
+    int N,a,b;
+    std::cout << "Input a Divisor for buzz" << '\n';
+    std::cin >> a;
+    std::cout << "Input a Divisor for fizz" << '\n';
+    std::cin >> b;
+    std::cout << "Input the value we will play to" << '\n';
     std::cin >> N;
     //iterate through all values until reaching the maximum value of N
-
     for(int i=0; i < N; i++)
         //checks for zero and places lets play fizzbuzz in its place
         if (i == 0)
@@ -22,21 +26,20 @@ int main()
             N = N +2;
         }
         else
-        //checks for fizzbuzz case 
-        if (i % 5 == 0 && i % 3 == 0)
+        //checks for fizzbuzz case
+        if (i % a == 0 && i % b == 0)
         {
             std::cout << "fizzbuzz" << '\n';
         }
         else
         //checks for fizz case
-        if (i % 3 == 0)
+        if (i % b == 0)
         {
             std::cout << "fizz" << '\n';
-             
         }
         else
-        //checks for buzz case 
-        if (i % 5 == 0)
+        //checks for buzz case
+        if (i % a == 0)
         {
             std::cout << "buzz" << '\n';
         }
